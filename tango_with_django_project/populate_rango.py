98e755a7d7885863cb3800de10a1369731bc9e9b
Robert Ranson <robbieranson@me.com>
2021-04-28 10:34:20 +0100

    chapter_6 tests passed

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -1,4 +1,6 @@
 import os
+import random
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE','tango_with_django.settings')
 
 import django
@@ -51,7 +53,7 @@ def populate():
 def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url = url
-    p.views=views
+    p.views= random.randrange(1,100)
     p.save()
     return p
 
